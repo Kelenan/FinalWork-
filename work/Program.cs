34e763faf36f89b353c1b7883ee36d6083a2ccd8
«Elena <leseee4ka@bk.ru>
2023-09-24 11:31:20 +0300

    secondArray

--- work/Program.cs
+++ work/Program.cs
@@ -3,4 +3,25 @@
  либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
  лучше обойтись исключительно массивами.*/
 
- 
+Console.Clear();
+
+string[] array1 = new string[4] {"Hello", "2", "world", "1"};
+string[] array2 = new string[array1.Length];
+
+SecondArray(array1, array2);
+PrintArray(array2);
+
+void SecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+
+
